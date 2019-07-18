8260bfc6a230de4415b63cd25d07d97c4d77f288
Alex Weeks <alextweeks@yahoo.co.uk>
2019-07-18 14:27:56 +0100

    Changes

--- src/App.js
+++ src/App.js
@@ -2,17 +2,25 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class NavItem extends Component  {
+  render() {
+    return (
+      <li class="nav-item active">
+        <a class="nav-link" href="#">{this.props.name}<span class="sr-only">(current)</span></a>
+      </li>
+    )
+  }
+}
+
 class NavBar extends Component {
   render() {
     return (
       <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-fixed-top">
-        <a class="navbar-brand" href="#">Navbar</a>
-        
+        <a class="navbar-brand" href="#">Testing</a>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
+            <NavItem name="Home" />
+            <NavItem name="Title" />
           </ul>
         </div>
       </nav>
