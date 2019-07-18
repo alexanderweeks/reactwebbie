92d9aa6d6d21dfd558e8fdc4ac0ae6f5cf98cfb1
alexanderweeks <alexandertweeks@gmail.com>
2019-07-18 16:52:28 +0100

    Messing around with layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './Content.css';
 
 class NavItem extends Component  {
   render() {
     return (
-      <li class="nav-item active">
-        <a class="nav-link" href="#">{this.props.name}<span class="sr-only">(current)</span></a>
+      <li className="nav-item active">
+        <a className="nav-link" href="#">{this.props.name}<span className="sr-only">(current)</span></a>
       </li>
     )
   }
@@ -15,10 +16,10 @@ class NavItem extends Component  {
 class NavBar extends Component {
   render() {
     return (
-      <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-fixed-top">
-        <a class="navbar-brand" href="#">Testing</a>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light navbar-fixed-top">
+        <a className="navbar-brand" href="#">Testing</a>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
             <NavItem name="Home" />
             <NavItem name="Title" />
           </ul>
@@ -40,11 +41,39 @@ class Header extends Component {
   }
 }
 
+class SlidingBox extends Component {
+  render() {
+    return (
+      <div className="section" id={this.props.topic}>
+        <div className="section-description">
+          <p>Hello World</p>
+        </div>
+        {/* <div className="vl" ></div> */}
+        <div className="section-picture">
+          Testing RHS
+        </div>
+      </div>
+    )
+  }
+}
+
+class Content extends Component {
+  render() {
+    return (
+      <div className="container-fluid content">
+        <SlidingBox topic="projects"/>
+        <SlidingBox topic="volunteering" />
+      </div>
+    )
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Content />
       </div>
     );
   }
