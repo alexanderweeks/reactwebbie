fe1b60b34b4b80e4f4aa16bc5eedfedc057f4e56
alexanderweeks <alexandertweeks@gmail.com>
2019-09-18 17:46:34 +0100

    Works Ish

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './Content.css';
+import './css/Resume.css';
 // import Header from './js/Header.js';
 import Nav from './js/Nav.js';
+import Container from './js/Container.js';
 
 class App extends Component {
   render() {
