b6b81da3499562ad10c880b77f1df56adfa66779
Alex Weeks <alextweeks@yahoo.co.uk>
2019-09-25 16:30:39 +0100

    Is this better dan

--- src/js/Nav.js
+++ src/js/Nav.js
@@ -1,28 +1,6 @@
 import React, { Component } from 'react';
 import ThumbnailImage from '../img/personal.JPG';
 
-class Personal extends Component {
-    render() {
-        return (
-            <a class="navbar-brand js-scroll-trigger" href="#page-top">
-                <span class="d-block d-lg-none">{this.props.name}</span>
-                <span class="d-none d-lg-block">
-                    <img class="img-fluid img-profile rounded-circle mx-auto mb-2" src={ThumbnailImage} alt="" />
-                </span>
-            </a>
-        )
-    }
-}
-class ToggleButton extends Component {
-    render() {
-        return (
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-        )
-    }
-}
-
 function getHref(value) {
     return "#" + value.toLowerCase();
 }
@@ -60,8 +38,15 @@ class Nav extends Component {
     render() {
         return (
             <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
-                <Personal name="Alex Weeks"/>
-                <ToggleButton />
+                <a class="navbar-brand js-scroll-trigger" href="#page-top">
+                <span class="d-block d-lg-none">Alex Weeks</span>
+                <span class="d-none d-lg-block">
+                    <img class="img-fluid img-profile rounded-circle mx-auto mb-2" src={ThumbnailImage} alt="" />
+                </span>
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
                 <Menu />
             </nav>
         )
