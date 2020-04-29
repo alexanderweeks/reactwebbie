5aca2c1e62cfab703c3aecbaa5c419df0166f0e8
Alex Weeks <alextweeks@yahoo.co.uk>
2020-04-29 22:21:49 +0100

    Scroll Footer Implemented

--- src/js/Main.js
+++ src/js/Main.js
@@ -7,7 +7,14 @@ class Main extends React.Component {
         return (
             <main>
                 <section className="hero">
-
+                    <div className="title">
+                        <h1>Alex Weeks</h1>
+                        <p className="subheading">it's just me being me</p>
+                    </div>
+                    <div className="footer">
+                        {/* <i className="fas fa-chevron-down"></i> */}
+                        <p className="scroll">scroll</p>
+                    </div>
                 </section>
                 <section className="section1">
 
